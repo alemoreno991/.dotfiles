1a88013043a9c63a64fbe0d26b7a4f546cfbba37
Alejandro Moreno <ale.moreno991@gmail.com>
2022-06-22 20:54:23 -0500

    Zsh Configuration
    I'm pretty safisfied with the zsh configuration
    
    USAGE:
    	bash install.sh
    
    TODO: document how the configuration works and where to look for
    help.

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,22 +4,23 @@
 echo "[-] Installing zsh [-]"
 sudo apt install zsh
 
+# Installing extra programs 
+echo "[-] Installing programs for enhanced experience [-]"
+sudo apt install ranger zoxide ncdu
+cd ~/Downloads && curl -fsSL https://fnm.vercel.app/install | bash
+
 # Make zsh your default shell
 echo "[-] Configuring zsh as your default shell [-]"
 # Configure start-up environment vars
-ln -s $(pwd)/zshenv $HOME/.zshenv
+ln -sf $(pwd)/zshenv $HOME/.zshenv
 # Configure zsh 
 MY_ZSH_CONFIG_PATH=$XDG_CONFIG_HOME/zsh
 mkdir -p $MY_ZSH_CONFIG_PATH
-ln -s $(pwd)/zshrc         $MY_ZSH_CONFIG_PATH/.zshrc        # Main config file
-ln -s $(pwd)/zsh-prompt    $MY_ZSH_CONFIG_PATH/zsh-prompt    # Prompt config
-ln -s $(pwd)/zsh-aliases   $MY_ZSH_CONFIG_PATH/zsh-aliases   # Aliases config
-ln -s $(pwd)/zsh-exports   $MY_ZSH_CONFIG_PATH/zsh-exports   # Exports config
-ln -s $(pwd)/zsh-vim-mode  $MY_ZSH_CONFIG_PATH/zsh-vim-mode  # Vim-like behavior
-ln -s $(pwd)/zsh-functions $MY_ZSH_CONFIG_PATH/zsh-functions # Minimal manager
-ln -s $(pwd)/completion    $MY_ZSH_CONFIG_PATH/completion    
+ln -sf $(pwd)/zshrc         $MY_ZSH_CONFIG_PATH/.zshrc        # Main config file
+ln -sf $(pwd)/zsh-prompt    $MY_ZSH_CONFIG_PATH/zsh-prompt    # Prompt config
+ln -sf $(pwd)/zsh-aliases   $MY_ZSH_CONFIG_PATH/zsh-aliases   # Aliases config
+ln -sf $(pwd)/zsh-exports   $MY_ZSH_CONFIG_PATH/zsh-exports   # Exports config
+ln -sf $(pwd)/zsh-vim-mode  $MY_ZSH_CONFIG_PATH/zsh-vim-mode  # Vim-like behavior
+ln -sf $(pwd)/zsh-functions $MY_ZSH_CONFIG_PATH/zsh-functions # Minimal manager
+ln -sf $(pwd)/completion    $MY_ZSH_CONFIG_PATH  
 chsh -s $(which zsh)
-
-# Installing extra programs 
-echo "[-] Installing programs for enhanced experience [-]"
-sudo apt install ranger zoxide fnm ncdu
