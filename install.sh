ec4b1b945869d15921c30459747431fe64a1f9cd
Alejandro Moreno <ale.moreno991@gmail.com>
2024-01-19 20:58:34 +0800

    Clean up

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ sudo apt install -y stow git
 stow . --target $HOME
 
 # Install my personal nvim configuration
-# ./.config/nvim/install.sh
+./.config/nvim/install.sh
 
 # Install my personal nvim configuration
-# ./.config/tmux/install.sh
+./.config/tmux/install.sh
 
 source $HOME/.bashrc
 
